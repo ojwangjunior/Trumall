2a24c6908754cd2463d96e5ae2ab22a8e24c64a5
ombima56 <ombimahillary6@gmail.com>
2025-09-06 02:00:20 +0300

    feat: add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import HomePage from './components/HomePage';
+import LoginPage from './components/LoginPage';
+import BuyPage from './components/BuyPage';
+import SellPage from './components/SellPage';
 
 function App() {
-
   return (
-    <>
-      <h1 className="text-3xl font-bold text-green-600">
-        Trumall Website
-      </h1>
-    </>
-  )
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/buy" element={<BuyPage />} />
+        <Route path="/sell" element={<SellPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
