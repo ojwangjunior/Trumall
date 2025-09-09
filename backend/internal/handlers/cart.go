5289d7e780f599a477f839d62a1091c0e6dd2431
Malika7188 <asmanodeny@gmail.com>
2025-09-10 01:09:25 +0300

    feat:Implement add to cart handler with stock validation

---
+++ backend/internal/handlers/cart.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+
+	"trumall/internal/models"
+)
+
+// Add product to cart
+func AddToCartHandler(db *gorm.DB) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		type request struct {
+			ProductID string `json:"product_id"`
+			Quantity  int    `json:"quantity"`
+		}
+		
