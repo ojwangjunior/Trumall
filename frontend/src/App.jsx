24d4a8daac168f2c1214ade530fb03c5374eff61
ombima56 <ombimahillary6@gmail.com>
2025-09-09 22:50:08 +0300

    feat: add productDetails route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import HomePage from './components/HomePage';
-import Footer from './components/Footer';
-import LoginPage from './components/LoginPage';
-import BuyPage from './components/BuyPage';
-import SellPage from './components/SellPage';
-import ProductsPage from './components/ProductsPage';
-import CartPage from './components/CartPage';
-import AboutPage from './components/AboutPage';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import HomePage from "./components/HomePage";
+import Footer from "./components/Footer";
+import LoginPage from "./components/LoginPage";
+import BuyPage from "./components/BuyPage";
+import SellPage from "./components/SellPage";
+import ProductsPage from "./components/ProductsPage";
+import CartPage from "./components/CartPage";
+import AboutPage from "./components/AboutPage";
+import ProductDetailPage from "./components/ProductDetailPage";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
             <Route path="/buy" element={<BuyPage />} />
             <Route path="/sell" element={<SellPage />} />
             <Route path="/products" element={<ProductsPage />} />
+            <Route path="/product/:id" element={<ProductDetailPage />} />
             <Route path="/cart" element={<CartPage />} />
             <Route path="/about" element={<AboutPage />} />
           </Routes>
