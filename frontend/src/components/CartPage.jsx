f25f55ae9eb6e87a7e732b62f4f869458c30d045
ombima56 <ombimahillary6@gmail.com>
2025-09-12 16:39:56 +0300

    chore: remove unused imports

--- frontend/src/components/CartPage.jsx
+++ frontend/src/components/CartPage.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { CartContext } from "../context/CartContext";
+import { CartContext } from "../context/cart";
 import { Link } from "react-router-dom";
 
 const CartPage = () => {
