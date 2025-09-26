6f45860df91c25b17116919fdcefd96211527ab9
ombima56 <ombimahillary6@gmail.com>
2025-09-26 16:48:51 +0300

    chore(auth): import AuthContext in ProtectedRoute

--- frontend/src/pages/SellPage.jsx
+++ frontend/src/pages/SellPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../context/auth-context";
 import { useToast } from "../context/ToastContext"; // Import useToast
 
 import SellPageHeader from "../components/sell/SellPageHeader";
