3446f32dd95f00c99b3c51b320131b10b24d3c52
ombima56 <ombimahillary6@gmail.com>
2025-09-26 16:48:39 +0300

    chore(auth): import AuthContext in ProtectedRoute

--- frontend/src/pages/SigninPage.jsx
+++ frontend/src/pages/SigninPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../context/auth-context";
 import { useNavigate } from "react-router-dom";
 import { useToast } from "../context/ToastContext"; // Import useToast
 
