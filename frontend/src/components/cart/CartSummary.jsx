8eb2d9e9fd318aefa2211ae16ad4e88feedfb54b
Malika7188 <asmanodeny@gmail.com>
2025-09-26 10:11:11 +0300

    implement handleCheckout function to trigger payment

--- frontend/src/components/cart/CartSummary.jsx
+++ frontend/src/components/cart/CartSummary.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const CartSummary = ({ calculateTotal, cartItems }) => {
+  const handleCheckout = async () => {
+  if (cartItems.length === 0) return;
+
+  
+ };
+
+
+ 
   return (
     <div className="mt-8 flex justify-end items-center">
       <div className="text-lg font-bold mr-4">
@@ -10,7 +19,7 @@ const CartSummary = ({ calculateTotal, cartItems }) => {
           currency: cartItems[0]?.Product.currency || "KES",
         }).format(calculateTotal() / 100)}
       </div>
-      <button className="px-6 py-3 font-semibold text-white bg-green-500 rounded-lg shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 transition-colors">
+      <button onClick={handleCheckout} className="px-6 py-3 font-semibold text-white bg-green-500 rounded-lg shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 transition-colors">
         Proceed to Checkout
       </button>
     </div>
