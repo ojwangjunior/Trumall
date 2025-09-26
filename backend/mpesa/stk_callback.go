aaf6d8862263db25cc5bb98442f9a3b631ee0674
Malika7188 <asmanodeny@gmail.com>
2025-09-26 09:23:30 +0300

    clean up metadata parsing with switch statement

--- backend/mpesa/stk_callback.go
+++ backend/mpesa/stk_callback.go
@@ -49,4 +49,5 @@ func StkCallbackHandler(dbConn *gorm.DB) fiber.Handler {
 			// Update DB
 			if err := dbConn.Model(&models.Payment{}).
 				Where("checkout_request_id = ?", sc.CheckoutRequestID).
-				
+				Updates(map[string]interface{}{
+					
